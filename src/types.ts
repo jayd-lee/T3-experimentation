1b60ae9717c20e71fe3c3e70eed1fcd15cd7fc87
Jayden Lee <jaydeh.lee@gmail.com>
2023-11-12 20:58:11 -0600

    Todos component, types from inferRouterOutputs

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
+import type { inferRouterOutputs } from '@trpc/server';
+import type { AppRouter } from '~/server/api/root';
 import { z } from 'zod'
+
+type RouterOutputs = inferRouterOutputs<AppRouter>
+type allTodosOutput = RouterOutputs['todo']['all']
+
+export type Todo = allTodosOutput[number]
 
 export const todoInput = z.string({
   required_error: 'Todo name required'
